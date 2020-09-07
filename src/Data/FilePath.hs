878f6887aeda890c7e70e66cba9e248740ef38d3
Andrey Mulik <work.a.mulik@gmail.com>
2020-09-07 20:39:17 +0300

    added missing functions from System.FilePath

--- src/Data/FilePath.hs
+++ src/Data/FilePath.hs
@@ -17,33 +17,38 @@
 -}
 module Data.FilePath
 (
-  -- * Separator predicates
-  FilePath, isPathSep, pattern PathSep,
+  -- * FilePath
+  FilePath, isPathSep, isValid, isRelative, isAbsolute,
+  
+  makeValid, normalise, equalFilePath, makeRelative,
   
   -- * @$PATH@
   getPath,
   
-  -- * Drive
+  -- * Patterns
+  pattern PathSep,
+  
+  -- ** Drive
   pattern (:\\),
   
-  -- * Extension functions
+  -- ** Extensions
   pattern (:.), pattern (:..),
   
-  -- * Filename\/directory functions
+  -- ** Split path
   pattern (:/), pattern (://), pattern Path, pattern Dirs
 )
 where
 
 import Prelude ()
 import SDP.SafePrelude hiding ( many )
-
 import SDP.Linear
 
 import Data.Bifunctor
+import Data.Function
+import Data.Char
 
 #ifndef IS_POSIX
 import Data.Maybe
-import Data.Char
 
 import Text.ParserCombinators.ReadPrec ( lift )
 import Text.ParserCombinators.ReadP
@@ -107,7 +112,6 @@ getPath =
   > "/" :. "d" == "/.d"
   
   Windows:
-  
   > "\\\\share" :. ".txt" == "\\\\share\\.txt"
   
   Split on the extension. Note that points are discarded.
@@ -194,11 +198,9 @@ splitExts path = case splitsBy (== '.') file' of
   > "./" :/ "bob" <- "bob"
   
   Posix:
-  
   > "/" :/ "" <- "/"
   
   Windows:
-  
   > "c:" :/ "" <- "c:"
   
   Combine two paths with a path separator.
@@ -209,7 +211,6 @@ splitExts path = case splitsBy (== '.') file' of
   > "directory" :/ "/file.ext" == "/file.ext"
   
   Posix:
-  
   > "/directory" :/ "file.ext" == "/directory/file.ext"
   > "/" :/ "tmp" == "/tmp"
   > "x:" :/ "foo" == "x:/foo"
@@ -217,7 +218,6 @@ splitExts path = case splitsBy (== '.') file' of
   > "home" :/ "user" == "home/user"
   
   Windows:
-  
   > "/directory" :/ "file.ext" == "/directory\\file.ext"
   > "C:\\foo" :/ "bar" == "C:\\foo\\bar"
   > "home" :/ "C:\\bob" == "C:\\bob"
@@ -233,7 +233,7 @@ splitExts path = case splitsBy (== '.') file' of
 pattern (:/) :: FilePath -> FilePath -> FilePath
 pattern dir :/ file <- (splitName -> (dir, file))
   where
-    a :/ b@(drive :\\ _) = headIs isPathSep b || drive /= "" ? b $ combine a b
+    a :/ b@(drive :\\ _) = headIs isPathSep b || drive /= "" ? b $ a :\\ b
 
 splitName :: FilePath -> (FilePath, FilePath)
 splitName =  first (\ dir -> null dir ? "./" $ dir) . dirName
@@ -245,7 +245,21 @@ dirName (drive :\\ path) = (drive ++) `first` breakr isPathSep path
 
 {-# COMPLETE Path #-}
 
--- | Separates filepath into a search path (list of ancestors and file name).
+{- |
+  Separates filepath into a search path - list of ancestors with trailing
+  separators and file name (if any):
+  
+  Posix:
+  > Path ["/", "home/", "user/", ".ghci"] <- "/home/user/.ghci"
+  > Path ["/", "home/", "user/"] <- "/home/user/"
+  > Path ["/", "home/", "user"] <- "/home/user"
+  
+  Windows:
+  > Path ["C:\\", "home\\", "user\\"] <- "C:\\home\\user\\"
+  > Path ["C:\\", "home\\", "user"] <- "C:\\home\\user"
+  
+  'Path' concatenates the file path regardless of a trailing separator.
+-}
 pattern Path :: [FilePath] -> FilePath
 pattern Path path <- (splitPath -> path) where Path = foldr (:/) []
 
@@ -260,7 +274,21 @@ splitPath (drive :\\ path) = null drive ? f path $ drive : f path
 
 {-# COMPLETE Dirs #-}
 
--- | Same as 'Path', but removes trailing path separators.
+{- |
+  Separates filepath into a search path - list of ancestors without trailing
+  separators and file name (if any):
+  
+  Posix:
+  > Dirs ["/", "home", "user", ".ghci"] <- "/home/user/.ghci"
+  > Dirs ["/", "home", "user"] <- "/home/user/"
+  > Dirs ["/", "home", "user"] <- "/home/user"
+  
+  Windows:
+  > Dirs ["C:\\", "home", "user"] <- "C:\\home\\user\\"
+  > Dirs ["C:\\","home","user"] <- "C:\\home\\user"
+  
+  'Dirs' concatenates the file path regardless of a trailing separator.
+-}
 pattern Dirs :: [FilePath] -> FilePath
 pattern Dirs dirs <- (splitDirs -> dirs) where Dirs = foldr (:/) []
 
@@ -285,7 +313,6 @@ pattern dirs :// file <- (splitDirs -> dirs :< file) where (://) = flip $ foldr 
 
 {- |
   Windows:
-  
   > "" :\\ "file" <- "file"
   > "c:/" :\\ "file" <- "c:/file"
   > "c:\\" :\\ "file" <- "c:\\file"
@@ -297,26 +324,22 @@ pattern dirs :// file <- (splitDirs -> dirs :< file) where (://) = flip $ foldr 
   > "" :\\ "/d" <- "/d"
   
   Posix:
-  
   > "/" :\\ "test" <- "/test"
   > "//" :\\ "test" <- "//test"
   > "" :\\ "test/file" <- "test/file"
   > "" :\\ "file" <- "file"
 -}
 pattern (:\\) :: FilePath -> FilePath -> FilePath
-pattern drive :\\ path <- (splitDrive -> (drive, path)) where (:\\) = combine
-
-combine :: FilePath -> FilePath -> FilePath
-combine a b
-  | null a = b
-  | null b = a
-  | isPathSep (last a) = a ++ b
-#ifdef IS_POSIX
-  | True = a ++ '/' : b
-#else
-  | c : ":" <- a, isLetter' c = a ++ b
-  | True = a ++ '\\' : b
+pattern drive :\\ path <- (splitDrive -> (drive, path))
+  where
+    a :\\ b
+      | null a = b
+      | null b = a
+      | isPathSep (last a) = a ++ b
+#ifndef IS_POSIX
+      | c : ":" <- a, isLetter' c = a ++ b
 #endif
+      | True = a ++ PathSep : b
 
 splitDrive :: FilePath -> (FilePath, FilePath)
 #ifdef IS_POSIX
@@ -329,9 +352,301 @@ splitDrive path =
 
 --------------------------------------------------------------------------------
 
+{- Validity. -}
+
+{- |
+  Is a 'FilePath' valid? This function checks for invalid names, invalid
+  characters, but doesn't check if length limits are exceeded, as these are
+  typically filesystem dependent.
+  
+  > isValid "" == False
+  > isValid "\0" == False
+  
+  Posix:
+  > isValid "/random_ path:*" == True
+  > isValid x => not (null x)
+  
+  Windows:
+  > isValid "c:\\test" == True
+  > isValid "c:\\test:of_test" == False
+  > isValid "test*" == False
+  > isValid "c:\\test\\nul" == False
+  > isValid "c:\\test\\prn.txt" == False
+  > isValid "c:\\nul\\file" == False
+  > isValid "\\\\" == False
+  > isValid "\\\\\\foo" == False
+  > isValid "\\\\?\\D:file" == False
+  > isValid "foo\tbar" == False
+  > isValid "nul .txt" == False
+  > isValid " nul.txt" == True
+-}
+isValid :: FilePath -> Bool
+isValid  ""  = False
+#ifdef IS_POSIX
+isValid path = not ('\0' `elem` path)
+#else
+isValid (drive :\\ path@(Dirs dirs)) = not $ or
+  [
+    any isBadChar path,
+    any (\ (name :.. _) -> isBadElem name) dirs,
+    
+    drive .>= 2 && all isPathSep drive,
+    isDriveUNC drive && not (lastIs isPathSep drive)
+  ]
+#endif
+
+{- |
+  Take a FilePath and make it vali, doesn't change already valid FilePaths:
+  
+  > isValid (makeValid x) == True
+  > isValid x => makeValid x == x
+  > makeValid "" == "_"
+  > makeValid "file\0name" == "file_name"
+  
+  Windows:
+  > makeValid "c:\\already\\/valid" == "c:\\already\\/valid"
+  > makeValid "c:\\test:of_test" == "c:\\test_of_test"
+  > makeValid "test*" == "test_"
+  > makeValid "c:\\test\\nul" == "c:\\test\\nul_"
+  > makeValid "c:\\test\\prn.txt" == "c:\\test\\prn_.txt"
+  > makeValid "c:\\test/prn.txt" == "c:\\test/prn_.txt"
+  > makeValid "c:\\nul\\file" == "c:\\nul_\\file"
+  > makeValid "\\\\\\foo" == "\\\\drive"
+  > makeValid "\\\\?\\D:file" == "\\\\?\\D:\\file"
+  > makeValid "nul .txt" == "nul _.txt"
+-}
+makeValid :: FilePath -> FilePath
+makeValid "" = "_"
+#ifdef IS_POSIX
+makeValid path = repl (== '\0') '_' path
+#else
+makeValid (drive :\\ path@(Path paths))
+    | drive .>= 2 && all isPathSep drive = take 2 drive ++ "drive"
+    | isDriveUNC drive && lastIs isPathSep drive =
+      makeValid (drive ++ "\\" ++ path)
+    | True = drive :\\ Path (f <$> paths)
+  where
+    f = uncurry ((++) . g) . break isPathSep . repl isBadChar '_'
+    g = \ x@(a :.. b) -> isBadElem a ? a ++ "_" :.. b $ x
+#endif
+
+--------------------------------------------------------------------------------
+
+{- Absolute and relative paths. -}
+
+{- |
+  Is a path relative, or is it fixed to the root?
+  
+  Posix:
+  > isRelative "test/path" == True
+  > isRelative "/test" == False
+  > isRelative "/" == False
+  
+  Windows:
+  
+  > isRelative "path\\test" == True
+  > isRelative "c:\\test" == False
+  > isRelative "c:test" == True
+  > isRelative "c:\\" == False
+  > isRelative "c:/" == False
+  > isRelative "c:" == True
+  > isRelative "\\\\foo" == False
+  > isRelative "\\\\?\\foo" == False
+  > isRelative "\\\\?\\UNC\\foo" == False
+  > isRelative "/foo" == True
+  > isRelative "\\foo" == True
+  
+  * "A UNC name of any format [is never relative]."
+  * "You can't use the "\\?\" prefix with a relative path."
+-}
+isRelative :: FilePath -> Bool
+#ifdef IS_POSIX
+isRelative (dr :\\ _) = null dr
+#else
+isRelative (dr :\\ _) = case dr of
+  (c : ':' : x : _) -> isLetter' c && not (isPathSep x)
+  [c, ':']          -> isLetter' c
+  xs                -> null xs
+#endif
+
+-- | Same as @not . 'isRelative'@.
+isAbsolute :: FilePath -> Bool
+isAbsolute =  not . isRelative
+
+--------------------------------------------------------------------------------
+
+{- |
+  Normalise a file name:
+  * \/\/ outside of the drive can be made blank
+  * \/ -> 'pathSeparator'
+  * .\/ -> \"\"
+  
+  > normalise "." == "."
+  
+  Posix:
+  > normalise "/file/\\test////" == "/file/\\test/"
+  > normalise "/file/./test" == "/file/test"
+  > normalise "/test/file/../bob/fred/" == "/test/file/../bob/fred/"
+  > normalise "../bob/fred/" == "../bob/fred/"
+  > normalise "./bob/fred/" == "bob/fred/"
+  
+  > normalise "./" == "./"
+  > normalise "./." == "./"
+  > normalise "/./" == "/"
+  > normalise "/" == "/"
+  > normalise "bob/fred/." == "bob/fred/"
+  > normalise "//home" == "/home"
+  
+  Windows:
+  > normalise "c:\\file/bob\\" == "C:\\file\\bob\\"
+  > normalise "c:\\" == "C:\\"
+  > normalise "C:.\\" == "C:"
+  > normalise "\\\\server\\test" == "\\\\server\\test"
+  > normalise "//server/test" == "\\\\server\\test"
+  > normalise "c:/file" == "C:\\file"
+  > normalise "/file" == "\\file"
+  > normalise "\\" == "\\"
+  > normalise "/./" == "\\"
+-}
+normalise :: FilePath -> FilePath
+normalise (drive :\\ path@(Dirs dirs)) = addPathSep ? res :< PathSep $ res
+  where
+    res = join' $ normDrive drive :\\ Dirs (f dirs)
+    
+    join' x = null x ? "." $ x
+    
+    addPathSep = isDirPath path
+                 && not (lastIs isPathSep res)
+#ifndef IS_POSIX
+                 && not (isRelativeDrive drive)
+#endif
+    
+    isDirPath xs = lastIs isPathSep xs ||
+                   lastIs (== '.') xs && lastIs isPathSep (init xs)
+    
+    f (x : xs) = except ("." ==) $ all isPathSep x ? [PathSep] : xs $ x : xs
+    f    []    = []
+
+normDrive :: FilePath -> FilePath
+#ifdef IS_POSIX
+normDrive dr = null dr ? "" $ [PathSep]
+#else
+normDrive "" = ""
+normDrive dr = isDriveLetter dosDrive ? map toUpper dosDrive $ dosDrive
+  where
+    dosDrive = repl (== '/') '\\' dr
+#endif
+
+{- |
+  Equality of two 'FilePath's. If you call @System.Directory.canonicalizePath@
+  first this has a much better chance of working. Note that this doesn't follow
+  symlinks or DOSNAMEs.
+  
+  > x == y ==> equalFilePath x y
+  > normalise x == normalise y ==> equalFilePath x y
+  
+  > equalFilePath "foo" "foo/"
+  > not (equalFilePath "foo" "/foo")
+  
+  Posix:
+  > not (equalFilePath "foo" "FOO")
+  
+  Windows:
+  > equalFilePath "foo" "FOO"
+  > not (equalFilePath "C:" "C:/")
+-}
+equalFilePath :: FilePath -> FilePath -> Bool
+equalFilePath =  on (==) $
+  dropSep .
+#ifndef IS_POSIX
+  map toLower .
+#endif
+  normalise
+
+dropSep :: FilePath -> FilePath
+dropSep xs = lastIs isPathSep xs && notDrive xs ? (null xs' ? [x] $ xs') $ xs
+  where
+    notDrive path@(_ :\\ rel) = null path || not (null rel)
+    
+    xs' = dropEnd isPathSep xs
+    x   = last xs
+
+--------------------------------------------------------------------------------
+
+{- |
+  Contract a filename, based on a relative path. Note that the resulting path
+  will never introduce @..@ paths, as the presence of symlinks means @..\/b@ may
+  not reach @a\/b@ if it starts from @a\/c@. For a worked example see
+  <http://neilmitchell.blogspot.co.uk/2015/10/filepaths-are-subtle-symlinks-are-hard.html this blog post>.
+  
+  The corresponding @makeAbsolute@ function can be found in @System.Directory@.
+  
+  > makeRelative "/directory" "/directory/file.ext" == "file.ext"
+  > Valid x => makeRelative (takeDirectory x) x `equalFilePath` takeFileName x
+  > makeRelative x x == "."
+  > Valid x y => equalFilePath x y || (isRelative x && makeRelative y x == x) || equalFilePath (y </> makeRelative y x) x
+  
+  Posix:
+  > makeRelative "/Home" "/home/bob" == "/home/bob"
+  > makeRelative "/home/" "/home/bob/foo/bar" == "bob/foo/bar"
+  > makeRelative "/fred" "bob" == "bob"
+  > makeRelative "/file/test" "/file/test/fred" == "fred"
+  > makeRelative "/file/test" "/file/test/fred/" == "fred/"
+  > makeRelative "some/path" "some/path/a/b/c" == "a/b/c"
+  
+  Windows:
+  > makeRelative "C:\\Home" "c:\\home\\bob" == "bob"
+  > makeRelative "C:\\Home" "c:/home/bob" == "bob"
+  > makeRelative "C:\\Home" "D:\\Home\\Bob" == "D:\\Home\\Bob"
+  > makeRelative "C:\\Home" "C:Home\\Bob" == "C:Home\\Bob"
+  > makeRelative "/Home" "/home/bob" == "bob"
+  > makeRelative "/" "//" == "//"
+-}
+makeRelative :: FilePath -> FilePath -> FilePath
+makeRelative root path
+    |   equalFilePath root path    = "."
+    | takeAbs root /= takeAbs path = path
+    |             True             = dropAbs root `f` dropAbs path
+  where
+    f "" y = dropWhile isPathSep y
+    f x  y = equalFilePath x1 y1 ? f x2 y2 $ path
+      where
+        (x1, x2) = g x
+        (y1, y2) = g y
+    
+    g = double bimap (dropWhile isPathSep) . break isPathSep . dropWhile isPathSep
+    
+    dropAbs pth@(drv :\\ rel) = headIs isPathSep pth && null drv ? tail pth $ rel
+    takeAbs pth@(drv :\\   _) = headIs isPathSep pth && null drv ? [PathSep] $ map (\ y -> isPathSep y ? PathSep $ toLower y) drv
+
+--------------------------------------------------------------------------------
+
 {- Windefs. -}
 
 #ifndef IS_POSIX
+isDriveLetter :: String -> Bool
+isDriveLetter x2 =  isJust (readMaybeBy (lift letter) x2)
+
+isDriveUNC :: String -> Bool
+isDriveUNC =  isJust . readMaybeBy (lift unc)
+
+isRelativeDrive :: String -> Bool
+isRelativeDrive =
+  maybe False (not . lastIs isPathSep . fst) . readMaybeBy (lift letter)
+
+isBadElem :: FilePath -> Bool
+isBadElem =  (`elem` badElems) . fmap toUpper . dropEnd (== ' ')
+
+badElems :: [FilePath]
+badElems =
+  [
+    "COM1", "COM2", "COM3", "COM4", "COM5", "COM6", "COM7", "COM8", "COM9",
+    "LPT1", "LPT2", "LPT3", "LPT4", "LPT5", "LPT6", "LPT7", "LPT8", "LPT9",
+    "CON",  "PRN",  "AUX",  "NUL",  "CLOCK$"
+  ]
+
+isBadChar :: Char -> Bool
+isBadChar x = x >= '\0' && x <= '\31' || x `elem` ":*?><|\""
 
 unc :: ReadP (String, String)
 unc =  do sep; sep; void (char '?'); sep; long <++ short
@@ -361,7 +676,6 @@ sep =  void (satisfy isPathSep)
 
 isLetter' :: Char -> Bool
 isLetter' x = isAsciiLower x || isAsciiUpper x
-
 #endif
 
 --------------------------------------------------------------------------------
@@ -371,5 +685,11 @@ headIs f es = not (null es) && f (head es)
 
 lastIs :: (Char -> Bool) -> String -> Bool
 lastIs f es = not (null es) && f (last es)
+
+repl :: (a -> Bool) -> a -> ([a] -> [a])
+repl =  \ f n -> map $ \ c -> f c ? n $ c
+
+double :: (a -> a -> b) -> a -> b
+double =  \ f x -> f x x
 
 
